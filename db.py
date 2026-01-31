9e395553dad030e982df858f09c8bcfa6221c349
Yusif Panahov <yusifpanahov@gmail.com>
2026-01-31 13:31:11 +0400

    init

---
+++ db.py
@@ -0,0 +1,8 @@
+users = [
+    {"id": 1, "name": "Alice"},
+    {"id": 2, "name": "Bob"},
+]
+
+products = [
+    {"id": 1, "name": "Laptop", "price": 10},
+    {"id": 2, "name": "Smartphone", "price": 5},]
